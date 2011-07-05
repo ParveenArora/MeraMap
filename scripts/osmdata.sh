11b2b997ceb00db9dbe089d554568441bf534270
Parveen Arora <prince.parveen@gmail.com>
2011-07-06 02:43:59 +0530

    Script Edited

--- scripts/osmdata.sh
+++ scripts/osmdata.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-if [ -d ~/planet ];
+if [ -d ~/plane ];
 then
     echo "File exists"
 else
-    mkdir ~/planet
-	if [ -f ~/planet/ludhiana.osm.bz2 ];
-	    then
-		echo "OSM Data File for Ludhiana exists"
-	    else 
-		cd ~/plan
-		wget https://github.com/ParveenArora/MeraMap/blob/master/osm_data_file/ludhiana.osm.bz2
-	fi
+    mkdir ~/plane
+
+fi
+
+if [ -f ~/planet/ludhiana.osm.bz2 ];
+then
+    echo "OSM Data File for Ludhiana exists"
+else 
+    cd ~/plane
+    wget https://github.com/ParveenArora/MeraMap/blob/master/osm_data_file/ludhiana.osm.bz2
 fi
