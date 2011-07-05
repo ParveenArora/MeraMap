11a2d0ddaaef428ddacd1c27068317179d4ba387
Parveen Arora <prince.parveen@gmail.com>
2011-07-06 01:16:15 +0530

    Script to Download OSM Data

---
+++ scripts/osmdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -d ~/planet ];
+then
+    echo "File exists"
+else
+    mkdir ~/planet
+	if [ -f ~/planet/ludhiana.osm.bz2 ];
+	    then
+		echo "OSM Data File for Ludhiana exists"
+	    else 
+		cd ~/plan
+		wget https://github.com/ParveenArora/MeraMap/blob/master/osm_data_file/ludhiana.osm.bz2
+	fi
+fi
