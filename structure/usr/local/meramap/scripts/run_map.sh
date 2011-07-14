c4dc6898cf51ecc7c0420cd37a7edfb9614be74d
Parveen Arora <prince.parveen@gmail.com>
2011-07-14 05:57:27 +0530

    Testing is being done, First 3 scripts working fine

---
+++ structure/usr/local/meramap/scripts/run_map.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source variables.sh
+if [ -d $PUBLIC_HTML ];
+then
+    cp -r map $PUBLIC_HTML
+else
+    mkdir public_html
+    cd /etc/apache2/mods-enabled
+    sudo ln -s ../mods-available/userdir.conf userdir.conf
+    sudo ln -s ../mods-available/userdir.load userdir.load
+    sudo /etc/init.d/apache2 restart
+fi
+cp -r map $PUBLIC_HTML
+
+echo For see the working of map, follow the given link. http://localhost/~$USERNAME/map
