9d6662c1208c82021e1dcf51e2298b9ba83e0c7d
Parveen Arora <prince.parveen@gmail.com>
2011-07-19 12:10:00 +0530

    Apache Defined in dependency, Previously missed If condition added

--- newmap/generate_map.sh
+++ newmap/generate_map.sh
@@ -2,8 +2,11 @@
 source variable.sh
 cp -r bin ~/
 
+if [ -f $PATH_MERAMAP ]; then rm -r $PATH_MERAMAP; fi
+
 sudo cp -r meramap /var/www
 
 sudo ln -s ~/bin/mapnik/tiles /var/www/OSMAP
+
 echo "Congratulations, You Can check your Map Working at http://localhost/meramap" 
 
