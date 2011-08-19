af32fbaacfd0cf0ae9980c42913852f3e7d62c76
Parveen Arora <prince.parveen@gmail.com>
2011-08-20 01:34:09 +0530

    Some more extra files removed

--- meramap/usr/local/meramap/scripts/mapnik_tiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source $MERAMAP_SCRIPT/variables.sh
-
-source $MERAMAP/bin/mapnik/archive/set-mapnik-env
-.$MERAMAP/bin/mapnik/archive/customize-mapnik-map > $MAPNIK_MAP_FILE
-.$MERAMAP/bin/mapnik/z0_generate_tiles.py
--- structure/usr/local/meramap/scripts/mapnik_tiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source variables.sh
-
-source /home/parveen/bin/mapnik/archive/set-mapnik-env
-sh /home/parveen/bin/mapnik/archive/customize-mapnik-map > $MAPNIK_MAP_FILE
-python /home/parveen/bin/mapnik/z0_generate_tiles.py
