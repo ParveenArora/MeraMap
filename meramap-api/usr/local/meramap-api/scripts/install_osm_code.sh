39d6f09de196d32b9c63c5f23c6ac2dd039c8410
Graham Jones <grahamjones139@gmail.com>
2011-08-26 22:29:58 +0100

    working on rails database configuration file set up, but struggling with sed.

--- meramap-api/usr/local/meramap-api/scripts/install_osm_code.sh
+++ meramap-api/usr/local/meramap-api/scripts/install_osm_code.sh
@@ -6,11 +6,16 @@ source $SCRIPTDIR/variables.sh
 cd $MERAMAP_API
 #git clone git://git.openstreetmap.org/rails.git
 
+DBTEMP=$MERAMAP_API/rails/config/postgres.example.database.yml
 DBCONF=$MERAMAP_API/rails/config/database.yml
 
-cp $MERAMAP_API/rails/config/postgres.example.database.yml $DBCONF
+#This first strips any leading white space or # character before username.
+#then replaces username with the desired value.
+sed -e 's/^[ \t#]*username//' -e 's/username./  username: $DBUSER #/g' $DBTEMP > $DBCONF
 
-sed -i -e 's/^username^/username: $DBUSER/g' $DBCONF
+#sed -e 's/.username./username: $DBUSER/g' $DBTEMP > $DBCONF
     
+#sed -e 's/^[ \t#]*//' -e 's/.username./NEWTEXT/g' postgres.example.database.yml
+
 
 echo "install_osm_code.sh complete!!"
