fb01c4a3a0de1b8b32df30087bb6a1f701c5921f
Graham Jones <grahamjones139@gmail.com>
2011-08-28 20:48:30 +0100

    Added copyright notices to scripts

--- meramap-api/usr/local/meramap-api/scripts/variables.sh
+++ meramap-api/usr/local/meramap-api/scripts/variables.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+############################################################################
+#    Copyright 2011 Graham Jones
+#
+#    This file is part of Meramap-api.
+#
+#    Meramap-api is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Meramap-api is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Meramap-api.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################
 MERAMAP_API=/usr/local/meramap-api     # MeraMap Directory in User's System
 
 LOGFILE_INST=$MERAMAP_API/logfiles/install.log
